f52ee00f9fc3f1d040f5f3f8b8227b0e1d53a066
Er206 <eloise.rd12@gmail.com>
2022-11-28 14:17:27 +0100

    V2

--- doc/Objects.py
+++ doc/Objects.py
@@ -1,23 +1,23 @@
 class Posts:
     def __init__(self, list_posts, time_posts,activityfield):
-        self.noms = list_posts
-        self.temps = time_posts[activityfield.indice]
+        self.name = list_posts
+        self.time = time_posts[activityfield.indice]
 
 class Post :
-    def __init__(self,Posts,indice)
-        self.nom = Posts.noms[indice]
-        self.temps = Posts.temps_postes[indice]
-        self.indice = indice
-        if indice > : 2
-            self.previous_task = posts[indice-1]
-        else :
+    def __init__(self,Posts,index):
+        self.name = Posts.name[index]
+        self.time = Posts.time_posts[index]
+        self.index = index
+        if index > 2:
+            self.previous_task = Posts.list_posts[index-1]
+        else:
             self.previous_task = "task 1"
 
 class ActivityField :
-    def __init__(self,nb_article_colis,indice,name):
+    def __init__(self,nb_article_package,index,name):
         self.name = name
-        self.nb_art = nb_article_colis
-        self.indice = indice
+        self.nb_art = nb_article_package
+        self.index = index
 
 class WorkOrg :
     def __init__(self,period_week, period_month, period_year):
@@ -32,18 +32,18 @@ class Operator :
         self.team = team
 
 class cle_repartition:
-    def __init__(self,jour,orga_travail):
-        self.periode = orga_travail
-        self.jour = jour
+    def __init__(self,day,WorkOrg):
+        self.period = WorkOrg
+        self.day = day
 
-semaine = ["lundi", "mardi", "mercredi", "jeudi", "vendredi","samedi", "dimanche"]
+week = ["monday", "tuesday", "wednesday", "thursday", "friday","saturday", "sunday"]
 
-nb_articles_colis = [1.24,2.3]
-temps_travail = 7*60*60
-temps_pause = 30*60
-indice_domaine_activité = [1,2]
-poste = ["pickeur rack", "pickeur etagere", "operateur skypod", "agent logistique ventilation", "agent logistique conduceteur de ligne emballage", "agent logistique palettisation"]
-equipe = [matin, apres_midi, soir]
-temps_poste_d2 = [25.7,48,10.3,18,16,10.6]
-temps_poste_d1 = [46.8,72,14.4,26.7,37.9,22.5]
-temps_poste = [temps_potse_d1,temps_poste_d2]
+nb_articles_package = [1.24,2.3]
+work_time = 7*60*60
+break_time = 30*60
+Activity_field_index = [1,2]
+post = ["pickeur rack", "pickeur etagere", "operateur skypod", "agent logistique ventilation", "agent logistique conduceteur de ligne emballage", "agent logistique palettisation"]
+team = ["morning", "after-noon", "night"]
+post_time_d2 = [25.7,48,10.3,18,16,10.6]
+post_time_d1 = [46.8,72,14.4,26.7,37.9,22.5]
+post_time = [post_time_d1,post_time_d2]
