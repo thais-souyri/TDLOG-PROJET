30a3b561dc44402ce9a1cd615d1929fe74cc83aa
Er206 <eloise.rd12@gmail.com>
2023-01-02 17:07:49 +0100

    bdd

--- plannificateur/database.py
+++ plannificateur/database.py
@@ -4,6 +4,17 @@ from datetime import date
 db = SqliteDatabase('people.db')
 
 
+class Person(Model):
+    ident = IntegerField(unique=True, primary_key=True)
+    name = CharField(unique=True)
+    availability = BooleanField()
+    nb_hour_week = IntegerField()   #nombre d'heures déja travaillées dans la semaine
+    nb_hour_day = IntegerField()    #nombre d'heures déja travaillées dans le jour
+
+    class Meta:
+        database = db
+
+
 class Post(Model):
     name = CharField(unique=True)
     time = FloatField()
@@ -13,11 +24,8 @@ class Post(Model):
         database = db
 
 
-class Person(Model):
-    name = CharField(unique=True)
-    availability = BooleanField()
-    nb_hour_week = IntegerField()   #nombre d'heures déja travaillées dans la semaine
-    nb_hour_day = IntegerField()    #nombre d'heures déja travaillées dans le jour
+class Skill(Model):
+    operator = ForeignKeyField(Person, backref='persons')
     post = ForeignKeyField(Post, backref='posts')
 
     class Meta:
@@ -41,11 +49,23 @@ picking = Post.create(name='picking', time=2, index=1)
 emballage = Post.create(name='emballage', time=1, index=2)
 palettisation = Post.create(name='palettisation', time=3, index=3)
 
-person1 = Person.create(name='one', post=picking, availability=True, nb_hour_week=0, nb_hour_day=0)
-person2 = Person.create(name='two', post=emballage, availability=True, nb_hour_week=0, nb_hour_day=0)
-person3 = Person.create(name='three', post=palettisation, availability=True, nb_hour_week=0, nb_hour_day=0)
-person4 = Person.create(name='four', post=picking, availability=True, nb_hour_week=0, nb_hour_day=0)
-person5 = Person.create(name='five', post=palettisation, availability=True, nb_hour_week=0, nb_hour_day=0)
+person1 = Person.create(ident=1, name='one', availability=True, nb_hour_week=0, nb_hour_day=0)
+person2 = Person.create(ident=2, name='two', availability=True, nb_hour_week=0, nb_hour_day=0)
+person3 = Person.create(ident=3, name='three', availability=True, nb_hour_week=0, nb_hour_day=0)
+person4 = Person.create(ident=4, name='four', availability=True, nb_hour_week=0, nb_hour_day=0)
+person5 = Person.create(ident=5, name='five', availability=True, nb_hour_week=0, nb_hour_day=0)
+
+skill1 = Skill(operator=1, post=picking)
+skill2 = Skill(operator=3, post=picking)
+skill3 = Skill(operator=5, post=picking)
+skill4 = Skill(operator=2, post=emballage)
+skill5 = Skill(operator=3, post=emballage)
+skill6 = Skill(operator=4, post=palettisation)
+skill7 = Skill(operator=5, post=palettisation)
+skill8 = Skill(operator=2, post=palettisation)
+skill9 = Skill(operator=1, post=palettisation)
+
+
 
 br = Activity.create(name='br', nb_packages=1, nb_article_packages=2.3,  day=date(2023,  7, 1))
 cd = Activity.create(name='cd', nb_packages=8, nb_article_packages=1.24, day=date(2023, 7, 1))
