158057deebbc7e3bc793fa0e7e4ef76e7565df60
Adeline Baillard <adelinebaillard@gmail.com>
2023-01-02 15:48:21 +0100

    site web version 1

--- app.py
+++ app.py
@@ -1,27 +1,23 @@
 from flask import Flask, render_template, request
+from plannificateur.constants import *
 
-app=Flask(__name__)
-
-
+app = Flask(__name__)
 
 
 @app.route("/")
-
 def index():
-    return render_template("index.html")
+    return render_template("index.html", entry=MONTHS)
+
 
 
 @app.route("/result", methods=["POST"])
-def result():
-    days = {"Lundi": 0, "Mardi": 0, "Mercredi": 0, "Jeudi": 0, "Vendredi": 0, "Samedi": 0}
-    return render_template("result.html",
-                           title="Résultats",
-                           results=days)
+def result2():
+    return render_template("result.html", days=DAYS, posts=POSTS,
+                           planning=PLANNING_EXAMPLE)
 
 
 if __name__ == '__main__':
     app.run(debug=True)
 
-#difficultés
-#afficher une image en arrière plan
-
+# difficultés
+# afficher une image en arrière plan
