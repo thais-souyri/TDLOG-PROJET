1a13cd757d312117d3fe3f6170df93063f9eea3d
adeli <adelinebaillard@gmail.com>
2023-01-15 16:53:23 +0100

    site avec authentification et base de données

--- app.py
+++ app.py
@@ -6,21 +6,33 @@ import os
 
 from plannificateur.tabledef import *
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin
-from flask_wtf import *
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
+from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import InputRequired, Length, ValidationError
+from flask_bcrypt import Bcrypt
 
 UPLOAD_FOLDER = '/Users/adeli/OneDrive'
 ALLOWED_EXTENSIONS = {'csv', 'txt'}
 
 app = Flask(__name__)
 db = SQLAlchemy(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/adeli/OneDrive/Bureau/Ponts2A/TDLOG/Projet/TDLOG-PROJET/database2.db'
+bcrypt = Bcrypt(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database2.db'
 app.config['SECRET_KEY'] = 'secret_key'
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
 
-class User2(db.Model, UserMixin):
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), nullable=False, unique=True)
     password = db.Column(db.String(80), nullable=False)
@@ -30,6 +42,19 @@ class RegisterForm(FlaskForm):
     username = StringField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder":"Username"})
     password = PasswordField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Password"})
     submit = SubmitField("Register")
+    def validate_username(self, username):
+        existing_user_username = User.query.filter_by(
+            username=username.data).first()
+
+        if existing_user_username:
+            raise ValidationError (
+                "Cet identifiant existe déjà. Veuillez en choisir un autre")
+
+
+class LoginForm(FlaskForm):
+    username = StringField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder":"Username"})
+    password = PasswordField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Password"})
+    submit = SubmitField("Login")
 
 
 
@@ -72,25 +97,44 @@ def launching():
     return render_template("launchingpage.html")
 
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
-    return render_template("login.html")
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user:
+            if bcrypt.check_password_hash(user.password, form.password.data):
+                login_user(user)
+                return redirect(url_for('index'))
+    return render_template("login.html", form=form)
 
 
-@app.route("/register")
+@app.route('/logout', methods=['GET', 'POST'])
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+
+
+@app.route("/register", methods=['GET', 'POST'])
 def register():
-    return render_template("register.html")
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data)
+        new_user = User(username=form.username.data, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('login'))
+
+    return render_template("register.html", form=form)
 
 
-@app.route("/index")
+@app.route("/index", methods=['POST', 'GET'])
+@login_required
 def index():
     return render_template("index.html", entry=MONTHS)
-
-
-# routeur "fantôme" pour pouvoir utiliser les méthodes POST et GET pour index
-@app.route("/setvolumetry", methods=["POST"])
-def vol():
-    return redirect(url_for("index"))
 
 
 @app.route("/result", methods=["POST"])
