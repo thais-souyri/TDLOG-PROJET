a16207362a0c0afed752ea3ad3db53e85b092211
Thaïs <thais.souyri@eleves.enpc.fr>
2023-01-18 00:44:21 +0100

    adaptation database

--- plannificateur/data.py
+++ plannificateur/data.py
@@ -1,18 +1,7 @@
-week = ["lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi", "dimanche"]
-months = ["janvier", "février", "mars", "avril", "mai", "juin", "juillet", "août", "septembre", "octobre", "novembre",
-          "décembre"]
+week = ["lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi"]
 
-nb_articles_package = [1.24, 2.3]
-work_time = 7 * 60 * 60
-break_time = 30 * 60
-activity_field_indexes = [1, 2]
-posts = ["pickeur rack", "pickeur etagere", "operateur skypod", "agent logistique ventilation",
-         "agent logistique conducteur de ligne emballage", "agent logistique palettisation"]
-nb_posts = len(posts)
 team = ["matin", "après-midi", "nuit"]
-post_time_d2 = [25.7, 48, 10.3, 18, 16, 10.6]
-post_time_d1 = [46.8, 72, 14.4, 26.7, 37.9, 22.5]
-post_time = [post_time_d1, post_time_d2]
+
 nb_max_team = 50
 
 max_height_database = 0
--- plannificateur/tools.py
+++ plannificateur/tools.py
@@ -13,15 +13,28 @@ def dict_creation() :
                 planning["{}".format(day)]["{}".format(team)]["{}".format(post.name)] = 0
     return planning
 
+
+
 def time_needed_post(nb_packages, activity_field,post):
     time_needed = nb_packages * database.Post.time[activity_field - 1][post.index]
     if post.action_on_article :
         time_needed = time_needed * activity_field.nb_article_package
     return time_needed
 
+def time_needed(nb_packages, activity_field,post):
+    sum = 0
+    for post in database.Post.select():
+        sum += time_needed_post(nb_packages, activity_field,post.name):
+    return post.name
+
 def persons_available():
     persons_available = database.Person.select().where(database.Person.nb_hour_day > 0 & database.Person.nb_hour_week > 0)
     return persons_available
+
+def persons_availbale_post(post):
+    persons_available = persons_available()
+    persons = persons_available.where(database.Skill.operrator == persons_available & database.Skill.post == post)
+    return persons
 
 def need_interim():
     persons = persons_available()
@@ -42,11 +55,14 @@ def nb_post():
         nb_post += 1
     return nb_post
 
-def time_needed_day(time_needed_day_team) :
-    time = 0
-    time_needed_day = []
-    for i in range (0,6):
-        for j in range (0,3):
-            time += time_needed_day_team[i][j]
-        time_needed_day. append(time)
-    return time_needed_day
+def total_operators(planning):
+    sum = 0
+    for i in range(0,6):
+        day = data.week[i]
+        for t in range (0,3):
+            team = data.team[t]
+            for post in database.Post.select() :
+                sum+= planning["{}".format(day)]["{}".format(team)]["{}".format(post.name)]
+    return sum
+
+
