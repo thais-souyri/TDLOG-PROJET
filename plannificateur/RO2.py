a9bf834a9c346d1ef3093208a4d0a90ded742d85
Thaïs <thais.souyri@eleves.enpc.fr>
2023-01-22 20:05:46 +0100

    plannificateur.

--- plannificateur/RO2.py
+++ plannificateur/RO2.py
@@ -3,17 +3,17 @@ import database
 import tools
 
 def planning(firm, nb_packages, nb_articles_package):
-    planning = tools.dict_creation(firm)
+    planning = plannificateur.tools.dict_creation(firm)
     nb_interim = []
 
     for post in database.Post.select().where(database.Post.firm_name == firm):
-        time_needed = tools.time_needed_post(firm, nb_packages, nb_articles_package, post)
+        time_needed = plannificateur.tools.time_needed_post(firm, nb_packages, nb_articles_package, post)
         nb_interim_post = 0
         nb_operators_night = 0
         time_operators_post = 0
-        persons_available = database.Person.select().where(database.Person.nb_hour_day < 7).where(database.Person.nb_hour_week < 35).where(database.Person.firm_name == firm)
+        persons_available = plannificateur.database.Person.select().where(plannificateur.database.Person.nb_hour_day < 7).where(plannificateur.database.Person.nb_hour_week < 35).where(plannificateur.database.Person.firm_name == firm)
         for person in persons_available:
-            persons_available_post = person.select().join(database.Skill).where(database.Skill.operator==person.ident).where(database.Skill.post == post.name).where(database.Skill.firm_name == firm)
+            persons_available_post = person.select().join(plannificateur.database.Skill).where(plannificateur.database.Skill.operator==person.ident).where(plannificateur.database.Skill.post == post.name).where(plannificateur.database.Skill.firm_name == firm)
             for p in persons_available_post:
                 time_operators_post += 35
 
@@ -25,48 +25,48 @@ def planning(firm, nb_packages, nb_articles_package):
         else :
             nb_interim.append(0)
 
-        if time_operators_post > 2*6*data.nb_max_team*7*60: # 2 équipes sur 6 jours travaillant 7h par jour
-            time_needed_night = time_operators_post - 2*6 * data.nb_max_team * 7*60
+        if time_operators_post > 2*6*plannificateur.data.nb_max_team*7*60: # 2 équipes sur 6 jours travaillant 7h par jour
+            time_needed_night = time_operators_post - 2*6 * plannificateur.data.nb_max_team * 7*60
             nb_operators_night = time_needed_night//(7*60) + 1
-            time_operators_post = 2*6 * data.nb_max_team*7*60
+            time_operators_post = 2*6 * plannificateur.data.nb_max_team*7*60
 
         nb_operators_needed_post = time_operators_post//(7*60) + nb_interim_post + 1
 
         for j in range(0, 6) :
-            day = data.week[j]
+            day = plannificateur.data.week[j]
             for k in range(0,2):
-                team = data.team[k]
+                team = plannificateur.data.team[k]
                 planning["{}".format(day)]["{}".format(team)]["{}".format(post.name)] += nb_operators_needed_post//12
         if nb_operators_needed_post % 12 > 6 :
             for j in range(0, 6):
-                day = data.week[j]
+                day = plannificateur.data.week[j]
                 for k in range(0, 2):
-                    team = data.team[k]
+                    team = plannificateur.data.team[k]
                     planning["{}".format(day)]["{}".format(team)]["{}".format(post)] += 1
             for j in range(0, int(nb_operators_needed_post % 12 - 6)):
-                day = data.week[j]
+                day = plannificateur.data.week[j]
                 for k in range(0, 2):
-                    team = data.team[k]
+                    team = plannificateur.data.team[k]
                     planning["{}".format(day)]["{}".format(team)]["{}".format(post)] += 1
         else :
             for j in range(0, int(nb_operators_needed_post % 12)):
-                day = data.week[j]
+                day = plannificateur.data.week[j]
                 for k in range(0, 2):
-                    team = data.team[k]
+                    team = plannificateur.data.team[k]
                     planning["{}".format(day)]["{}".format(team)]["{}".format(post)] += 1
 
 
         for d in range(0, 6):
-            day = data.week[d]
-            team = data.team[2]
+            day = plannificateur.data.week[d]
+            team = plannificateur.data.team[2]
             planning["{}".format(day)]["{}".format(team)]["{}".format(post)] += nb_operators_night // 6
 
         for j in range(0,int(nb_operators_night%6)):
-            day = data.week[j]
-            team = data.team[2]
+            day = plannificateur.data.week[j]
+            team = plannificateur.data.team[2]
             planning["{}".format(day)]["{}".format(team)]["{}".format(post)] += 1
 
     sum_interim = sum(nb_interim)//5 + 1
-    nb_operators = tools.total_operators(firm, planning)//5 + 1
+    nb_operators = plannificateur.tools.total_operators(firm, planning)//5 + 1
     return (planning, sum_interim, nb_operators)
 
--- plannificateur/tools.py
+++ plannificateur/tools.py
@@ -4,19 +4,19 @@ import database
 def dict_creation(firm) :
     planning = {}
     for j in range(0, 6):
-        day = data.week[j]
+        day = plannificateur.data.week[j]
         planning["{}".format(day)] = {}
         for k in range(0, 3):
-            team = data.team[k]
+            team = plannificateur.data.team[k]
             planning["{}".format(day)]["{}".format(team)] = {}
-            for post in database.Post.select().where(database.Post.firm_name == firm):
+            for post in plannificateur.database.Post.select().where(plannificateur.database.Post.firm_name == firm):
                 planning["{}".format(day)]["{}".format(team)]["{}".format(post.name)] = 0
     return planning
 
 
 
 def time_needed_post(firm, nb_packages, nb_articles_package,post):
-    for post in database.Post.select().where(database.Post.name == post).where(database.Post.firm_name == firm):
+    for post in plannificateur.database.Post.select().where(database.Post.name == post).where(plannificateur.database.Post.firm_name == firm):
         t = nb_packages * post.time
         if post.action_on_article :
             time = t * nb_articles_package
@@ -26,22 +26,22 @@ def time_needed_post(firm, nb_packages, nb_articles_package,post):
 
 def time_needed(firm, nb_packages, nb_articles_package):
     sum = 0
-    for post in database.Post.select().where(database.Post.firm_name == firm):
+    for post in plannificateur.database.Post.select().where(plannificateur.database.Post.firm_name == firm):
         sum += time_needed_post(firm,nb_packages, nb_articles_package,post.name)
     return sum
 
 def persons_available(firm):
-    persons_available = database.Person.select().where(database.Person.nb_hour_day < 7).where(database.Person.nb_hour_week < 35).where(database.Person.firm_name == firm)
+    persons_available = plannificateur.database.Person.select().where(plannificateur.database.Person.nb_hour_day < 7).where(plannificateur.database.Person.nb_hour_week < 35).where(plannificateur.database.Person.firm_name == firm)
     return persons_available
 
 def persons_available_post(firm, post):
     p_a = persons_available(firm)
-    persons = p_a.select().join(database.Skill).where(database.Skill.operator == persons_available).where(database.Skill.post == post).where(database.Skill.firm_name == firm)
+    persons = p_a.select().join(plannificateur.database.Skill).where(plannificateur.database.Skill.operator == persons_available).where(plannificateur.database.Skill.post == post).where(plannificateur.database.Skill.firm_name == firm)
     return persons
 
 def need_interim(firm,post):
-    persons_available = database.Person.select().where(database.Person.nb_hour_day < 7).where(database.Person.nb_hour_week < 35).where(database.Person.firm_name == firm)
-    persons_available_post = persons_available.select().join(database.Skill).where(database.Skill.operator == persons_available).where(database.Skill.post == post).where(database.Skill.firm_name == firm)
+    persons_available = plannificateur.database.Person.select().where(dplannificateur.atabase.Person.nb_hour_day < 7).where(plannificateur.database.Person.nb_hour_week < 35).where(plannificateur.database.Person.firm_name == firm)
+    persons_available_post = persons_available.select().join(plannificateur.database.Skill).where(plannificateur.database.Skill.operator == persons_available).where(plannificateur.database.Skill.post == post).where(plannificateur.database.Skill.firm_name == firm)
     sum = 0
     need = False
     for p in persons_available_post:
@@ -54,23 +54,23 @@ def need_interim(firm,post):
 
 def nb_person(firm):
     nb_person = 0
-    for person in database.Person.select().where(database.Person.firm_name == firm):
+    for person in plannificateur.database.Person.select().where(plannificateur.database.Person.firm_name == firm):
         nb_person += 1
     return nb_person
 
 def nb_post(firm):
     nb_post = 0
-    for post in database.Post.select().where(database.Post.firm_name == firm):
+    for post in plannificateur.database.Post.select().where(plannificateur.database.Post.firm_name == firm):
         nb_post += 1
     return nb_post
 
 def total_operators(firm, planning):
     sum = 0
     for i in range(0,6):
-        day = data.week[i]
+        day = plannificateur.data.week[i]
         for t in range (0,3):
             team = data.team[t]
-            for post in database.Post.select().where(database.Post.firm_name == firm) :
+            for post in plannificateur.database.Post.select().where(plannificateur.database.Post.firm_name == firm) :
                 sum += planning["{}".format(day)]["{}".format(team)]["{}".format(post.name)]
     return sum
 
