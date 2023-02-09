9209c3dc13882b4b5f8592071191715c398554a3
adeli <adelinebaillard@gmail.com>
2023-02-09 19:20:05 +0100

    images relatives v6

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+# importations
+
 import os
 from flask import Flask, render_template, request, redirect, url_for, flash, send_from_directory
 from flask_bcrypt import Bcrypt
@@ -17,6 +19,8 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
 
+# Création du fichier pour uploader les fichiers csv
+
 path = os.getcwd()
 # file Upload
 UPLOAD_FOLDER = os.path.join(path, 'uploads')
@@ -26,19 +30,39 @@ if not os.path.isdir(UPLOAD_FOLDER):
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+# types de fichiers autorisés
 ALLOWED_EXTENSIONS = {'csv'}
 
 
-# pour uploader les fichiers
-def allowed_file(filename):
-    return '.' in filename and \
-        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+# fonctions permettant de gérer l'authentification
+
+# charge une instance de User à partir d'une ID user
+@login_manager.user_loader
+def load_user(user_id):
+    return model.database.User.get(user_id)
 
 
+@app.before_request
+def before_request():
+    model.database.db.connect()
+
+
+@app.after_request
+def after_request(response):
+    model.database.db.close()
+    return response
+
+
+@login_manager.unauthorized_handler
+def unauthorized():
+    return "You must be logged in to access this page."
+
+
+# crée le firm name à partir du nom d'utilisateur si l'utilisateur est connecté
 @app.route('/create-firm-name')
 def create_firm_name():
     if current_user.is_authenticated:
-        user = database.User.get(username=current_user.username)
+        user = model.database.User.get(username=current_user.username)
         user.firm_name = "My Firm"
         user.save()
         return "Firm name created"
@@ -46,6 +70,19 @@ def create_firm_name():
         return "Not logged in"
 
 
+# permet de rediriger vers la page de lancement
+@app.route("/")
+def launching():
+    return render_template("launchingpage.html")
+
+
+# vérifie si un fichier est au format csv
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+# permet à l'utilisateur d'uploader ses fichiers csv dans le dossier prévu
 @app.route('/upload', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
@@ -64,7 +101,7 @@ def upload_file():
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             return redirect(url_for('download_file', name=filename))
 
-    return render_template('upload.html')
+    return render_template('first_login.html')
 
 
 @app.route('/uploads/<name>')
@@ -72,33 +109,7 @@ def download_file(name):
     return send_from_directory(app.config["UPLOAD_FOLDER"], name)
 
 
-@app.route('/index', methods=['POST', 'GET'])
-def index():
-    return render_template('index.html')
-
-
-# base de données pour les utilisateurs
-
-
-@login_manager.user_loader
-def load_user(user_id):
-    return database.User.get(user_id)  # TODO: charger une instance de User à partir d'une ID user
-
-
-@app.before_request
-def before_request():
-    database.db.connect()
-
-
-@app.after_request
-def after_request(response):
-    database.db.close()
-    return response
-
-
-# the user model specifies its fields (or columns) declaratively
-
-
+# crée le nom d'utilisateur et le mot de passe
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
@@ -107,29 +118,55 @@ def register():
         # hash the password
         hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
         # store the user in the database
-        database.User.create(username=username, password=hashed_password)
-        return redirect('/login')
+        model.database.User.create(username=username, password=hashed_password)
+        return redirect('/first')
     return render_template('register.html')
 
 
+# on upload les fichiers à la première connexion
+@app.route('/first', methods=['GET', 'POST'])
+def first_login():
+    return redirect('/upload')
+
+
+@app.route('/to_login', methods=['GET', 'POST'])
+def to_login():
+    return render_template('login.html')
+
+
+# permet à l'utilisateur de se connecter
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
         try:
-            user = database.User.get(database.User.username == username)
+            user = model.database.User.get(model.database.User.username == username)
+
             # check if the password is correct
             if bcrypt.check_password_hash(user.password, password):
                 login_user(user)
-                return redirect('/upload')
+                # création des bases de données
+                file_name1 = "person.csv"
+                file_path1 = os.path.join(app.config['UPLOAD_FOLDER'], file_name1)
+                model.database.create_table_person(file_path1, current_user.username)
+                file_name2 = "post.csv"
+                file_path2 = os.path.join(app.config['UPLOAD_FOLDER'], file_name2)
+                model.database.create_table_post(file_path2, current_user.username)
+                file_name3 = "skill.csv"
+                file_path3 = os.path.join(app.config['UPLOAD_FOLDER'], file_name3)
+                model.database.create_table_skill(file_path3, current_user.username)
+
+                return render_template('package.html')
             else:
                 return 'Incorrect password'
-        except database.User.DoesNotExist:
+        except model.database.User.DoesNotExist:
             return 'Incorrect username'
+
     return render_template('login.html')
 
 
+# permet de se déconnecter
 @app.route("/logout")
 @login_required
 def logout():
@@ -137,44 +174,21 @@ def logout():
     return redirect("/")
 
 
-@login_manager.unauthorized_handler
-def unauthorized():
-    return "You must be logged in to access this page."
-
-
-@app.route('/uploadusername', methods=['GET', 'POST'])
-@login_required
-def upload_page():
-    return f"L'utilisateur actuel est {current_user.username}"
-    pass  # TODO
-
-
+# permet de rediriger vers la page à propos de nous
 @app.route('/us')
 def us():
     return render_template('us.html')
 
 
-@app.route("/")
-def launching():
-    return render_template("launchingpage.html")
-
+# utilise les infos de l'utilisateur et redirige vers la page de planning
 @app.route("/result", methods=["POST", "GET"])
 def result():
-    # création des bases de données
-    file_name1 = "person.csv"
-    file_path1 = os.path.join(app.config['UPLOAD_FOLDER'], file_name1)
-    database.create_table_person(file_path1, current_user.username)
-    file_name2 = "post.csv"
-    file_path2 = os.path.join(app.config['UPLOAD_FOLDER'], file_name2)
-    database.create_table_post(file_path2, current_user.username)
-    file_name3 = "skill.csv"
-    file_path3 = os.path.join(app.config['UPLOAD_FOLDER'], file_name3)
-    database.create_table_skill(file_path3, current_user.username)
+    
+
     colis = int(request.form['nb_colis'])
     pieces = int(request.form['nb_pieces'])
     # utilisation de la fonction RO de création de planning
     resultat = plannificateur.RO3.planning(current_user.username, colis, pieces)
-
 
     return render_template("result.html", days=DAYS, posts=POSTS2,
                            planning=resultat[0], nb_person=resultat[2], nb_interim=resultat[1])
